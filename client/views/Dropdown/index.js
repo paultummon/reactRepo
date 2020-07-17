e237ae6f21b8323ecf6d86e0726175342aed9bd1
Paul Tummon <paultummon@lt-d788jw2.corp.local>
2020-07-17 13:58:23 +0100

    added dropdown view

--- client/views/Dropdown/index.js
+++ client/views/Dropdown/index.js
@@ -6,18 +6,42 @@ const Banner = styled.div`
     display: flex;
     justify-content: center;
     width: 100%;
-    color: white;
+    color: #949391;
+    font-family: Gotham SSm A,Gotham SSm B,arial,sans-serif;
     height 50px;
     align-items: center;
-    background-color: purple;
+    background-color: #e3e1f7;
 `;
 
+// const Container = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     min-height: 1000px;
+//     background-image: url(../../../public/img/porsche.jpg);
+//     width: 100%;
+//     filter: grayscale(100%);
+//     background-color: green;
+// `
+
 const Container = styled.div`
+    display: flex;
     justify-content: center;
-    align-items: center;
+    min-height: 1000px;
+    background-image: url(../../../public/img/porsche.jpg);
     width: 100%;
-    min-height: 100%;
     background-color: green;
+`
+const ListView = styled.ul`
+    justify-content: center;
+    color: red;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+`
+
+const CenterContainer = styled.div`
+    display: flex;
+
 `
 
 const Button = styled.button`
@@ -37,6 +61,27 @@ const Button = styled.button`
   }
 `;
 
+const ListItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-width: 1000px;
+  background-color: grey;
+  border-radius: 10px;
+  height: 50px;
+  margin: 10px
+`
+const arrayOfNames = ['steve', 'bucky', 'josh', 'rocket', 'groot', 'natasha']
+
+const renderListItems = () => {
+    return <ListView> {arrayOfNames.map(name => {
+    return <ListItem>Hello Marvel Character: {name}</ListItem>
+    })}</ListView>
+}
+
+
+
 export default class Dropdown extends React.Component {
     constructor(props){
         super(props)
@@ -51,8 +96,9 @@ export default class Dropdown extends React.Component {
                 <Banner>Dropdown View</Banner>
                 
                 <Container>
-                    <Button>Click Me</Button>
+                    {renderListItems()}
                 </Container>
+                <Button>Click Me</Button>
             </div>
         )
     }
